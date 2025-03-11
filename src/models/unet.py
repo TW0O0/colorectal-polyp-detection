7b23b38e8821c846e2d8f868a118564d503ee1a8
TW0O0 <kushreddy05@gmail.com>
2025-03-10 21:55:44 -0400

    Update unet.py

--- src/models/unet.py
+++ src/models/unet.py
@@ -1,1 +1,130 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+class DoubleConv(nn.Module):
+    """
+    Double Convolution block with batch normalization.
+    (conv -> batch norm -> ReLU) * 2
+    """
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """
+    Downscaling with maxpool then double conv
+    """
+    def __init__(self, in_channels, out_channels):
+        super(Down, self).__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """
+    Upscaling then double conv
+    """
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up, self).__init__()
+
+        # Use bilinear interpolation or transpose conv
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
+
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        
+        # input is BCHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        
+        # Concatenate along the channels axis
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    """
+    Output convolution
+    """
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    """
+    Full U-Net architecture
+    """
+    def __init__(self, n_channels=3, n_classes=1, bilinear=True, features=[64, 128, 256, 512]):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        # Initial double convolution
+        self.inc = DoubleConv(n_channels, features[0])
+        
+        # Downsampling path
+        self.down1 = Down(features[0], features[1])
+        self.down2 = Down(features[1], features[2])
+        self.down3 = Down(features[2], features[3])
+        
+        # Bottom of U-Net
+        factor = 2 if bilinear else 1
+        self.down4 = Down(features[3], features[3] * 2 // factor)
+        
+        # Upsampling path
+        self.up1 = Up(features[3] * 2, features[3] // factor, bilinear)
+        self.up2 = Up(features[3], features[2] // factor, bilinear)
+        self.up3 = Up(features[2], features[1] // factor, bilinear)
+        self.up4 = Up(features[1], features[0], bilinear)
+        
+        # Output convolution
+        self.outc = OutConv(features[0], n_classes)
+
+    def forward(self, x):
+        # Contracting path
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        
+        # Expanding path
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        
+        # Output
+        logits = self.outc(x)
+        return logits
